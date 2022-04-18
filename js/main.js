3b7eff3ee1b9eac4f855a8e63bb64d92e650d8f9
AlinaAzovtseva <johninn666@gmail.com>
2022-04-18 14:35:07 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-	$('.menu-burger').click(function(event) {
-		$('.menu-burger,.menu__list').toggleClass('active');
-	})
-});
+// $(document).ready(function() {
+// 	$('.menu-burger').click(function(event) {
+// 		$('.menu-burger,.menu__list').toggleClass('active');
+// 	})
+// });
+
+const burger = document.querySelector('.menu-burger');
+if (burger) {
+	const menuList = document.querySelector('.menu__list');
+	burger.addEventListener("click" ,function (e) {
+		burger.classList.toggle('active');
+		menuList.classList.toggle('active');
+	});
+}
